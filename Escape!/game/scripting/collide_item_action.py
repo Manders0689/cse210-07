4ad3ae3924556edfc4c9d0ed62f4573397cc6288
kharman14 <harman.katelyn96@gmail.com>
2022-07-15 16:48:48 -0600

    changes to collide Items action file

--- Escape!/game/scripting/collide_item_action.py
+++ Escape!/game/scripting/collide_item_action.py
@@ -12,20 +12,12 @@ class CollideItemAction(Action):
         self._audio_service = audio_service
         
     def execute(self, cast, script, callback):
-        #ball = cast.get_first_actor(BALL_GROUP)
-        items = cast.get_actors(ITEM_GROUP)
-        stats = cast.get_first_actor(STATS_GROUP)
-        message = cast.get_message(MESSAGE_GROUP)
+        item = cast.get_first_actor(ITEM_GROUP)
+        message = item.get_message()
+        character = cast.get_first_actor(CHARACTER_GROUP)
         
-        for item in items:
-            #ball_body = ball.get_body()
-            item_body = item.get_body()
+        character_body = character.get_body()
+        item_body = item.get_body()
 
-            if self._physics_service.has_collided(item_body):
-                #ball.bounce_y()
-                #sound = Sound(BOUNCE_SOUND)
-                #self._audio_service.play_sound(sound)
-                #points = brick.get_points()
-                #stats.add_points(points)
-                #cast.remove_actor(BRICK_GROUP, brick)
-                stats.get_message(message) ##Is this in stats?
+        if self._physics_service.has_collided(item_body, character_body):
+                item.get_message(message)
