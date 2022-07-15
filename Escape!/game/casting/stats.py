d9890948db3052fdc78be4f9c9b92b775f473898
cloggerish <104546948+cloggerish@users.noreply.github.com>
2022-07-15 15:19:36 -0600

    updated stats

--- Escape!/game/casting/stats.py
+++ Escape!/game/casting/stats.py
@@ -4,10 +4,12 @@
 
 from constants import *
 from game.casting.actor import Actor
-
+import time
+import datetime
 
 class Stats(Actor):
-    """The game stat including level."""
+    """The game stat including level and 
+    remaining time on timer."""
 
     def __init__(self, debug = False):
         """Constructs a new Stats."""
@@ -15,6 +17,24 @@ class Stats(Actor):
         self._level = 1
         self._timer = TIME_LIMIT
 
+    def set_timer(self, total_seconds):
+        while self._timer > 0:
+        
+            # Timer represents time left on countdown
+            timer = datetime.timedelta(seconds = total_seconds)
+                
+            # Prints the time left on the timer
+            # self.set_text(f"Timer: {timer}")
+            # print(timer, end="\r")
+        
+            # Delays the program one second
+            time.sleep(1)
+        
+            # Counts timer down by 1 second
+            total_seconds -= 1
+        
+        return timer
+        
     def get_level(self):
         """Gets the level.
 
@@ -24,11 +44,11 @@ class Stats(Actor):
         return self._level
   
     def get_timer(self):
-#         """Gets the time.
+        """Gets the time.
 
-#         Returns:
-#             A number representing the time.
-#         """
+        Returns:
+            A number representing the number of seconds left.
+        """
         return self._timer
     
     def next_level(self):
