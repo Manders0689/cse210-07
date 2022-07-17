c7dcba67a1b9b1553eec4aee2c59b7cad8547dd8
kharman14 <harman.katelyn96@gmail.com>
2022-07-16 19:15:41 -0600

    for loop

--- Escape!/game/scripting/collide_item_action.py
+++ Escape!/game/scripting/collide_item_action.py
@@ -12,17 +12,18 @@ class CollideItemAction(Action):
         self._audio_service = audio_service
         
     def execute(self, cast, script, callback):
-        item = cast.get_first_actor(ITEM_GROUP)
+        items = cast.get_actor(ITEM_GROUP)
         #message = item.get_message()
         character = cast.get_first_actor(CHARACTER_GROUP)
-        door = cast.get_first_actor(DOOR_GROUP)
-        
         character_body = character.get_body()
-        item_body = item.get_body()
+        door = cast.get_first_actor(DOOR_GROUP)
 
-        if self._physics_service.has_collided(item_body, character_body):
-                # needs to print to screen
-                item.get_message()
-                # to save that we have found the key
-                if item.get_key() == True:
-                    door.set_key(True)
+        for item in items:
+            item_body = item.get_body()
+
+            if self._physics_service.has_collided(item_body, character_body):
+                    # needs to print to screen
+                    item.get_message()
+                    # to save that we have found the key
+                    if item.get_key() == True:
+                        door.set_key(True)
